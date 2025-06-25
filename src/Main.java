76398422484de790c608274412e94cda52dbc0af
Marcos_desktop <jorgede1097@gmail.com>
2025-06-25 14:49:57 -0300

    palindromo

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,10 @@
-import java.util.Arrays;
-
+import structures.Arrays;
 public class Main {
     public static void main (String[] arg){
-        System.out.println("Hello, World!");
-        int[] lista = {1,2,3,4};
-        int alvo = 7;
-        int [] resultado = twoSum(lista,alvo);
-        System.out.println(Arrays.toString(resultado));
-    }
+        String s1 = "arara";
+        boolean result1 = Arrays.palindromoValido(s1);
+        System.out.println(result1);
 
-    public static int[] twoSum(int[] arr, int target){
-        int left = 0;
-        int right = arr.length -1;
-        int[] resultado = {};
 
-        while (left < right){
-            int soma = arr[left]+arr[right];
-
-            if (soma == target){
-                resultado = new int[]{arr[left], arr[right]};
-                return resultado;
-            } else if (soma < target) {
-                left += 1;
-            }else{
-                right -= 1;
-            }
-        }
-        return resultado;
     }
 }
